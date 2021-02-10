0ab8fc2a4543c3f18d9eb41c5c8f2641d79d1a8c
Patrick Roelofs <contact@patrickroelofs.com>
2021-02-10 11:50:40 +0100

    update

--- les 5/exercise-streams-master/src/main/java/nl/han/ica/oose/dea/Streams.java
+++ les 5/exercise-streams-master/src/main/java/nl/han/ica/oose/dea/Streams.java
@@ -11,32 +11,35 @@ import java.util.stream.Collectors;
 class Streams {
 
     List<String> filterStringsShorterThanThreeCharacters(List<String> input) {
-        // TODO: implement
-        return input;
+        return input.stream()
+                .filter(inString -> inString.length() < 3)
+                .collect(Collectors.toList());
     }
 
     List<String> filterStringsThatContainOnlyNumerals(List<String> input) {
-        // TODO: implement
-        return input;
+        return input.stream()
+                .filter(inString -> inString.matches("[0-9]+"))
+                .collect(Collectors.toList());
     }
 
     String findShortestString(List<String> input) {
-        // TODO: implement
-        return "";
+        return input.stream()
+                .min(Comparator.comparingInt(String::length))
+                .orElse(null);
     }
 
     String createAFullSentenceFromTheList(List<String> input) {
-        // TODO: implement
-        return "";
+        return String.join(" ", input);
     }
 
     int calculateTotalCostOfAllProducts(List<Product> products) {
-        // TODO: implement
-        return 0;
+        return products.stream()
+                .mapToInt(Product::getPrice).sum();
     }
 
     int calculateTotalCostOfAllGadgets(List<Product> products) {
-        // TODO: implement
-        return 0;
+        return products.stream()
+                .filter(inProduct -> inProduct.getCategory() == ProductCategory.GADGETS)
+                .mapToInt(Product::getPrice).sum();
     }
 }
