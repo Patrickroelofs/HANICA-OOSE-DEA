c83816b495ba5421789af4f06567953d1e205e0a
Patrick Roelofs <contact@patrickroelofs.com>
2021-03-15 10:55:20 +0100

    update Spotitube

--- Spotitube/src/main/java/com/spotitube/controller/dto/TokenDTO.java
+++ Spotitube/src/main/java/com/spotitube/controller/dto/TokenDTO.java
@@ -11,6 +11,11 @@ public class TokenDTO {
     this.token = generateToken();
   }
 
+  public TokenDTO(String user, String token) {
+    this.user = user;
+    this.token = token;
+  }
+
   public TokenDTO() {
 
   }
